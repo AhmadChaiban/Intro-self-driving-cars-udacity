206f8c0c1671cb03e3ab7cb69552dd7b12fa4ab8
Ahmad Chaiban <ahmadchaiban@gmail.com>
2020-08-18 19:01:59 +0300

    Section 5 - Lesson 2 - Completed

---
+++ Section 5 - Performance Programming in C++/Lesson 2 - C++ Optimization Practice/code/Static/blur_factor_improved.cpp
@@ -0,0 +1,22 @@
+#include "blur_factor_improved.hpp"
+
+using namespace std;
+
+vector < vector <float> > blur_factor_improved() {
+
+    // calculate blur factors
+    static float center = 1.0 - .12;
+    static float corner = .12 / 12.0;
+    static float adjacent = .12 / 6.0;
+    
+    // create the blur window
+
+    static vector< vector <float> > window = {
+            {corner, adjacent, corner},
+            {adjacent, center, adjacent},
+            {corner, adjacent, corner}
+    };
+
+    return window;
+}
+
